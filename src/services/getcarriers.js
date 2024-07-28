23bce150e8bd647fa72c0bc3d46bb5f3412dc8fa
Milagros <gmilagrosmj@gmail.com>
2024-07-28 15:25:43 +0200

    actualizacion de ruta

--- src/services/getcarriers.js
+++ src/services/getcarriers.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configura la URL base de tu API
 export const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/v1/',
+    baseURL: 'https://babckend-yf-3.onrender.com/api/v1/',
 });
 
 // Función para obtener la lista de carriers
